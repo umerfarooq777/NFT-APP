e109dfe9cd997d0148bb0c9361db95d29e1f2f1a
Umer Farooq <98796369+umerfarooq777@users.noreply.github.com>
2022-09-12 17:45:46 +0500

    initial project ptimized

---
+++ screens/Details.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyleSheet, Text, View ,StatusBar} from 'react-native';
+
+const Details = () => {
+  return (
+    <Text>Home</Text>
+  )
+}
+
+export default Details
